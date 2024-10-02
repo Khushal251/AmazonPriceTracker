0e3537cc2416711a852887bf6a158d33ae910c87
Khushal Kumawat <114289491+Khushal251@users.noreply.github.com>
2024-10-02 22:22:30 +0530

    Add files via upload

---
+++ AmazonPriceTracker/app/_layout.tsx
@@ -0,0 +1,21 @@
+import '../global.css';
+
+import { Stack } from 'expo-router';
+
+import AuthContextProvider from '~/contexts/AuthContext';
+
+export const unstable_settings = {
+  // Ensure that reloading on `/modal` keeps a back button present.
+  initialRouteName: '(tabs)',
+};
+
+export default function RootLayout() {
+  return (
+    <AuthContextProvider>
+      <Stack>
+        <Stack.Screen name="(app)/(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="(app)/modal" options={{ presentation: 'modal' }} />
+      </Stack>
+    </AuthContextProvider>
+  );
+}
